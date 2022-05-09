743560540c80a53b3c2fab7c961006a2f86de091
theoldtree <yjeongmu@gmail.com>
2022-05-09 19:24:41 +0900

    install basic

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import styled from "styled-components/native";
+
+const TextDefault = styled.Text`
+  font-size: 20px;
+  align-self: center;
+`;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <TextDefault>Hello World</TextDefault>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
