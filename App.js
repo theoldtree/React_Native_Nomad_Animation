d16222c49937de3c16842eadc87e45217e8c1467
theoldtree <yjeongmu@gmail.com>
2022-05-10 12:51:22 +0900

    add animation

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components/native";
+import { Animated } from "react-native";
 
-const TextDefault = styled.Text`
-  font-size: 20px;
-  align-self: center;
+const Container = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
 `;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
-  return <TextDefault>Hello World</TextDefault>;
+  const Y = useRef(new Animated.Value(0)).current;
+  const [up, setUp] = useState(false);
+  const toggleUp = () => setUp((prev) => !prev);
+  const goUp = () => {
+    Animated.timing(Y, {
+      toValue: up ? 200 : -200,
+      useNativeDriver: true,
+    }).start(toggleUp);
+  };
+  return (
+    <Container>
+      <AnimatedBox
+        onPress={goUp}
+        style={{
+          transform: [{ translateY: Y }],
+        }}
+      />
+    </Container>
+  );
 }
